4a1482febc90f7a7f48b280a80f99971b1b98142
D3v0ps <Karim.khalil002@gmail.com>
2026-05-07 01:06:42 +0200

    Update config.js

--- bajram-admin/config.js
+++ bajram-admin/config.js
@@ -3,5 +3,5 @@
 // eller så kan du publicera en separat Web App-deployment för admin.
 // Exempel: https://script.google.com/macros/s/.../exec?view=admin
 window.ADMIN_CONFIG = {
-  adminWebAppUrl: "PASTE_ADMIN_WEB_APP_URL_HERE"
+  adminWebAppUrl: "https://script.google.com/macros/s/AKfycbwYXgZ6PirSdCa8aKTpyT0RLnw8TkCpk4zwdSZWSvTD2ENy1Vqh26nxS6u0m7xVF_nP7g/exec?view=admin"
 };
